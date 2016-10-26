31933dc064269978bf10263e4f26f18d1eac4901
vincent.zhou <sicong_zhou@163.com>
2016-10-26 16:59:11 +0800

    addd

--- WebRoot/js/index/deployecharts.js
+++ WebRoot/js/index/deployecharts.js
@@ -207,7 +207,7 @@ var echartsvm = avalon.define({
 
                     }
                     //TODO
-                    echartsvm.option.title.text = "每月发布次数 (自然年)";
+                    echartsvm.option.title.text = "每月发布次数 ("+tempYear+"年)";
                     var mounthlyChart = echarts.init(document.getElementById('mounthlychart'));
                     mounthlyChart.setOption(echartsvm.option);
 
@@ -242,7 +242,7 @@ var echartsvm = avalon.define({
                     echartsvm.appOption.xAxis[0].data = echartsvm.MounthArr;
                     echartsvm.appOption.series[0].data = data.count;
                     //TODO
-                    echartsvm.appOption.title.text = "每月发布应用数 (自然年)";
+                    echartsvm.appOption.title.text = "每月发布应用数 ("+tempYear+"年)";
                     var mounthlyAppChart = echarts.init(document.getElementById('mounthlyAppchart'));
                     mounthlyAppChart.setOption(echartsvm.appOption);
                 } else {
@@ -302,7 +302,7 @@ var echartsvm = avalon.define({
                     }
 
                     //TODO
-                    echartsvm.option.title.text = "每日发布次数 (自然月)";
+                    echartsvm.option.title.text = "每日发布次数 ("+tempYear+"年"+tempMonth+"月)";
                     var dailyChart = echarts.init(document.getElementById('dailychart'));
                     dailyChart.setOption(echartsvm.option);
 
@@ -341,7 +341,7 @@ var echartsvm = avalon.define({
                     echartsvm.appOption.xAxis[0].data = echartsvm.DailyArr;
                     echartsvm.appOption.series[0].data = data.count;
                     //TODO
-                    echartsvm.appOption.title.text = "每日发布应用数 (自然月)";
+                    echartsvm.appOption.title.text = "每日发布应用数 ("+tempYear+"年"+tempMonth+"月)";
                     var dailyAppChart = echarts.init(document.getElementById('dailyAppchart'));
                     dailyAppChart.setOption(echartsvm.appOption);
 
